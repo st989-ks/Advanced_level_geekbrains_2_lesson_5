ae4d45498dc5744bf31379cbc27f4676a8640581
Stanislav <st989@ya.ru>
2020-10-05 21:27:31 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long a = System.currentTimeMillis();
+
+        System.out.println(System.currentTimeMillis() - a);
+
+    }
+}
