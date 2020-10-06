231562be2ebb4717497cec1bfa3ee21f660143d4
Stanislav <st989@ya.ru>
2020-10-06 21:26:54 +0400

    HW_5 final

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 import java.util.Arrays;
 
 public class Main {
-    private static final int size = 10000000;
+    private static final int size = 10_000_000;
     private static final int h = size / 2;
     private static float[] arr = new float[size];
 
-    public static void replacement(float[] arr) {
+    // в методе replacement, boolean True для одного потока (или для первого потока) False для второго потока
+    public static void replacement(float[] arr, boolean x) {
+
+        //y для второго потока так как size / 2 = 5_000_000
+        int y;
+        y = x ? 0 : h;
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin( 0.2f + i / 5 )
-                    * Math.cos( 0.2f + i / 5 ) * Math.cos( 0.4f + i / 2 ));
+            arr[i] = (float) (arr[i] * Math.sin( 0.2f + (i + y) / 5 )
+                    * Math.cos( 0.2f + (i + y) / 5 ) * Math.cos( 0.4f + (i + y) / 2 ));
         }
-
     }
 
-    public static void replacementSpeed() {
+    public static void replacementSpeed() throws InterruptedException {
 
         float[] a1 = new float[h];
         float[] a2 = new float[h];
@@ -22,50 +26,30 @@ public class Main {
         System.arraycopy( arr, 0, a1, 0, h );
         System.arraycopy( arr, h, a2, 0, h );
 
+        Thread thread1 = new Thread( () -> replacement( a1, true ) );
+        Thread thread2 = new Thread( () -> replacement( a2, false ) );
 
-
-        Thread thread1 = new Thread( new Runnable() {
-            @Override
-            public void run() {
-                replacement( a1 );
-            }
-        });
-        Thread thread2 = new Thread( new Runnable() {
-            @Override
-            public void run() {
-                replacement( a2 );
-            }
-        });
-
-        thread2.start();
         thread1.start();
+        thread2.start();
 
-//        new Thread( () -> replacement( a1 ) ).start();
-//        new Thread( () -> replacement( a2 ) ).start();
+        thread1.join();
+        thread2.join();
 
         System.arraycopy( a1, 0, arr, 0, h );
         System.arraycopy( a2, 0, arr, h, h );
-
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Arrays.fill( arr, 1 );
         long a = System.currentTimeMillis();
-        replacement( arr );                                     // один поток
-        System.out.println( System.currentTimeMillis() - a );
+        replacement( arr, true );
+        System.out.println( "Один поток: " + (System.currentTimeMillis() - a) );
 
-//        Arrays.fill( arr, 1 );
-//        a = System.currentTimeMillis();
-//        new Thread( () -> replacement( arr ) ).start();
-//        System.out.println( System.currentTimeMillis() - a );
-
-//        Arrays.fill( arr, 1 );
-//        a = System.currentTimeMillis();
-//        replacementSpeed();
-//        System.out.println( System.currentTimeMillis() - a );
-
-
+        Arrays.fill( arr, 1 );
+        a = System.currentTimeMillis();
+        replacementSpeed();
+        System.out.println( "Многопоточность: " + (System.currentTimeMillis() - a) );
     }
 
 
